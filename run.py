5a2a2ed6e4bf31fce0aedbb51130647863bc3c0e
Tomizap <88057971+Tomizap@users.noreply.github.com>
2023-05-23 15:07:06 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from pprint import pprint
 from selenium_scrapper import selenium_scrapper
 from selenium_driver import selenium_driver
 
-data = selenium_scrapper(driver=selenium_scrapper(), url=f"https://www.linkedin.com/jobs/search/?currentJobId=3612871351&geoId=103029567&keywords=marketing&location=Clichy%2C%20%C3%8Ele-de-France%2C%20France&refresh=true")
+data = selenium_scrapper(driver=selenium_driver(), url=f"https://www.linkedin.com/jobs/search/?currentJobId=3612871351&geoId=103029567&keywords=marketing&location=Clichy%2C%20%C3%8Ele-de-France%2C%20France&refresh=true")
 pprint(data)
